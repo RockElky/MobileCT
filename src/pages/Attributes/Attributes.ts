c4bf80eb68cbef1e361729c7ab2b2aaa3f6e27ca
Erik Oehlke <Erik Oehlke>
2017-04-10 22:02:13 -0500

    Finally got it working again.

---
+++ src/pages/Attributes/Attributes.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'Attributes',
+  templateUrl: 'Attributes.html'
+})
+export class Attributes {
+
+  constructor(public navCtrl: NavController) {
+
+  
+  }
+}
