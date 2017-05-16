9329f87e3271b363341dddbd577ce11ee3d1c2c2
Erik Oehlke <eaoehlke@impromed.com>
2017-05-16 11:35:48 -0500

    changed attributes

--- src/pages/Attributes/Attributes.ts
+++ src/pages/Attributes/Attributes.ts
@@ -8,22 +8,54 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'Attributes.html'
 })
 export class Attributes {
-private AttrPoints = 4;
+  private AgiPoints = 4;
+  private StrPoints = 4;
+  private TenPoints = 4;
+  private IntPoints = 4;
+  private PerPoints = 4;
+  private PresPoints = 4;
+  private CoordPoints = 4;
+  private BrawnPoints = 4;
+  private WellPoints = 4;
+  private InsPoints = 4;
+  private IntuitPoints = 4;
+  private CharPoints = 4;
   constructor(public navCtrl: NavController) {
 
-  
+
   }
-  increment() {
-    this.AttrPoints++;
+  increment(iagi,istr,iten,iint,iper,ipres,icoord,ibrawn,iwell,iins,iintu,ichar) {
+    iagi = this.AgiPoints++;
+    istr = this.StrPoints++;
+    iten = this.TenPoints++;
+    iint = this.IntPoints++;
+    iper = this.PerPoints++;
+    ipres = this.PresPoints++;
+    icoord = this.CoordPoints++;
+    ibrawn = this.BrawnPoints++;
+   iwell = this.WellPoints++;
+    iins = this.InsPoints++;
+    iintu = this.IntuitPoints++;
+    ichar = this.CharPoints++;
   }
-  decrement()
-  {
-    this.AttrPoints--;
+  decrement(dagi,dstr,dten,dint,dper,dpres,dcoord,dbrawn,dwell,dins,dintu,dchar) {
+    dagi = this.AgiPoints--;
+    dstr = this.StrPoints--;
+    dten = this.TenPoints--;
+    dint = this.IntPoints--;
+    dper = this.PerPoints--;
+    dpres = this.PresPoints--;
+   dcoord = this.CoordPoints--;
+    dbrawn = this.BrawnPoints--;
+    dwell = this.WellPoints--;
+    dins = this.InsPoints--;
+    dintu = this.IntuitPoints--;
+    dchar = this.CharPoints--;
   }
-    Characteristics() {
-   this.navCtrl.setRoot(Characteristics);
+  Characteristics() {
+    this.navCtrl.setRoot(Characteristics);
   }
-   Race() {
-   this.navCtrl.setRoot(Race);
+  Race() {
+    this.navCtrl.setRoot(Race);
   }
 }
--- src/pages/Characteristics/Characteristics.ts
+++ src/pages/Characteristics/Characteristics.ts
@@ -8,15 +8,44 @@ import { NavController } from 'ionic-angular';
     templateUrl: 'Characteristics.html'
 })
 export class Characteristics {
-    private AttrPoints = 4;
+    private ActionPoints = 4;
+    private DamagePoints = 4;
+    private RefPoints = 4;
+    private WitPoints = 4;
+    private ResiPoints = 4;
+    private TenaPoints = 4;
+    private OrgPoints = 4;
+    private ComPoints = 4;
+    private EvePoints = 4;
+    private LongPoints = 4;
+
     constructor(public navCtrl: NavController) {
-        
+
     }
     increment() {
-        this.AttrPoints++;
+        this.ActionPoints++;
+        this.DamagePoints++;
+        this.RefPoints++;
+        this.WitPoints++;
+        this.ResiPoints++;
+        this.TenaPoints++;
+        this.OrgPoints++;
+        this.ComPoints++;
+        this.EvePoints++;
+        this.LongPoints++;
+        
     }
     decrement() {
-        this.AttrPoints--;
+        this.ActionPoints--;
+        this.DamagePoints--;
+        this.RefPoints--;
+        this.WitPoints--;
+        this.ResiPoints--;
+        this.TenaPoints--;
+        this.OrgPoints--;
+        this.ComPoints--;
+        this.EvePoints--;
+        this.LongPoints--;
     }
 
     Attributes() {
